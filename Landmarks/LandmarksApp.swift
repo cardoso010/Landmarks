95f0da56d8949f3066400f7a5d36d4426369ec2c
Gabriel Cardoso Luiz <gabriel.cardoso010@gmail.com>
2021-01-26 17:52:25 -0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Gabriel Cardoso on 26/01/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
