f2fa0b156c702869e80381655f73ddfb7beb45b3
Gabriel Cardoso Luiz <gabriel.cardoso010@gmail.com>
2021-01-27 18:52:53 -0300

    Building Lists and Navigation

---
+++ Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,34 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Gabriel Cardoso on 27/01/21.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarks) { landmark in
+                NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                    LandmarkRow(landmark: landmark)
+                }
+                
+            }
+            .navigationTitle("Landmarks")
+        }
+        
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        ForEach(["iPhone SE", "iPhone XS Max"], id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
+        
+    }
+}
