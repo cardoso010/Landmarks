1b4e5a1266e36f9bfa7834d4339a8302f4d48158
Gabriel Cardoso Luiz <gabriel.cardoso010@gmail.com>
2021-01-28 13:07:42 -0300

    Handling User Input

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Gabriel Cardoso on 26/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environmentObject(ModelData())
+    }
+}
